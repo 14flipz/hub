417996a97e69d7d94f5795a9c93be5cb260cfda8
flipz <80792529+14flipz@users.noreply.github.com>
2023-02-12 21:11:41 -0500

    Update niggahub.lua

--- niggahub.lua
+++ niggahub.lua
@@ -7,13 +7,28 @@ local Noclipping = nil
 RunService = game:GetService("RunService")
 
     local function NoclipLoop()
-			for _, child in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
-				if child:IsA("BasePart") and child.CanCollide == true then
-					child.CanCollide = false
-				end
-			end
-			task.wait()
+            for _, child in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                if child:IsA("BasePart") and child.CanCollide == true then
+                    child.CanCollide = false
+                end
+            end
+            task.wait()
     end
+    local oreNames = {"Prairie", "PrairieCave", "VolcanoQuarry", "Desert"}
+local oreIndex = 1
+
+local function teleportToOre()
+    local oreFolder = game:GetService("Workspace").Ores:FindFirstChild(oreNames[oreIndex])
+    if oreFolder then
+        local orePart = oreFolder:GetChildren()[1]:FindFirstChild("Part")
+        if orePart then
+            local player = game:GetService("Players").LocalPlayer
+            local teleportCFrame = orePart.CFrame
+            player.Character:SetPrimaryPartCFrame(teleportCFrame + Vector3.new(0, 0, 4), orePart.Position)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.lookAt(game.Players.LocalPlayer.Character.HumanoidRootPart.Position, orePart.Position)
+        end
+    end
+end
 
 local chestFolder = game:GetService("Workspace").Chests
 
@@ -38,17 +53,14 @@ local function teleportToRandomChest()
         if game.Players.LocalPlayer.Character.Humanoid.Health == 100 then
             task.wait(10)
             collectChest()
-            task.wait(2)
         else
             collectChest()
-            task.wait(2)
         end
     end
 end
 
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
-local venyx = library.new("Nigger Hub", 5013109572)
-
+local venyx = library.new("Hunter Hub", 5013109572)
 
 local themes = {
     Background = Color3.fromRGB(24, 24, 24),
@@ -59,51 +71,58 @@ local themes = {
     TextColor = Color3.fromRGB(255, 255, 255)
 }
 
-
 local page = venyx:addPage("Main", 5012544693)
 local section1 = page:addSection("Chest Farm")
-
-section1:addToggle("No Fall Damage", false, function(value)
-    fallDamageEnabled = value
-    while fallDamageEnabled do
-        game.Players.LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.FallingDown, false)
-        game.Players.LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Freefall, false)
-        game.Players.LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.Jumping, false)
-        task.wait()
-    end
-end)
 
 section1:addToggle("Auto Chest Farm", false, function(value)
     while true do
         if value == true then
             teleportToRandomChest()
+            task.wait(1.5)
         else
             break
         end
     end
 end)
 
-section1:addToggle("Float", false, function(value)
-    while value do
-        pcall(function()
-local Float = Instance.new('Part', game.Workspace)
-Float.Name = 'nigger'
-Float.Transparency = 1
-Float.Size = Vector3.new(2,0.2,1.5)
-Float.Anchored = true
-local FloatValue = -3.1
-Float.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,FloatValue,0)
-task.wait()
+section1:addToggle("Auto Ore", false, function(boolean)
+    getgenv().AutoOre = boolean
+    while AutoOre do
+        teleportToOre()
+            game:GetService("Players").LocalPlayer.Character:FindFirstChild("Old Pickaxe").Slash:FireServer({[1] = 1})
+            task.wait()
+    end
 end)
-end
+
+local FloatLoop = nil
+
+section1:addToggle("Float", false, function(value)
+    if value then
+        FloatLoop = RunService.Heartbeat:Connect(function()
+            local Float = Instance.new('Part', game.Workspace)
+            Float.Name = 'hutner'
+            Float.Transparency = 1
+            Float.Size = Vector3.new(2,0.2,1.5)
+            Float.Anchored = true
+            local FloatValue = -3.1
+            Float.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0,FloatValue,0)
+        end)
+    else
+        if FloatLoop then
+            FloatLoop:Disconnect()
+        end
+    end
 end)
 
 section1:addButton("No-Clip", function(value)
     Noclipping = RunService.Stepped:Connect(NoclipLoop)
 end)
 
+section1:addButton("Teleport To Ore", function()
+    teleportToOre()
+end)
+
 for theme, color in pairs(themes) do
     venyx:setTheme(theme, color)
-end
-
+    end
 venyx:SelectPage(venyx.pages[1], true)
